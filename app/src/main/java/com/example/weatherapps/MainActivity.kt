7643230219fff85f2c04f9f7b3fdac9fdd0a2661
Shuvo-Roy <shuvoroysp@gmail.com>
2021-12-29 13:26:31 +0600

    Frontend

---
+++ app/src/main/java/com/example/weatherapps/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
